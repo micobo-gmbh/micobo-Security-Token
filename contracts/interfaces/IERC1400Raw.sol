7c6b320b9d5129e97b73cf040d47f7c3406bdcd2
Simon Dosch <sdosch2110@gmail.com>
2020-05-19 14:06:28 +0200

    add erc1400raw transfer tests

--- contracts/interfaces/IERC1400Raw.sol
+++ contracts/interfaces/IERC1400Raw.sol
@@ -19,14 +19,13 @@ interface IERC1400Raw {
   // deleted (TODO add again in administrable)
   // function controllers() external view returns (address[] memory); // 6/13
 
-  // not necessary for ERC1400Partition
   function authorizeOperator(address operator) external; // 7/13
   function revokeOperator(address operator) external; // 8/13
   function isOperator(address operator, address tokenHolder) external view returns (bool); // 9/13
 
-  // not necessary for ERC1400Partition
-  function transferWithData(address to, uint256 value, bytes calldata data) external; // 10/13
-  function transferFromWithData(address from, address to, uint256 value, bytes calldata data, bytes calldata operatorData) external; // 11/13
+  // overridden in ERC1400Partition
+  // function transferWithData(address to, uint256 value, bytes calldata data) external; // 10/13
+  // function transferFromWithData(address from, address to, uint256 value, bytes calldata data, bytes calldata operatorData) external; // 11/13
 
   // not possible with ERC1400Partition
   // function redeem(uint256 value, bytes calldata data) external; // 12/13
