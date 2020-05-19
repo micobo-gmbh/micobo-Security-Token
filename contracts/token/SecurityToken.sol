39f66375477fd9881799d22ea688e78930f98ff4
Daniel Gretzke <daniel@gretzke.de>
2020-05-19 17:07:03 +0200

    removed document requirement

--- contracts/token/SecurityToken.sol
+++ contracts/token/SecurityToken.sol
@@ -126,8 +126,6 @@ contract SecurityToken is ERC1400ERC20, IERC1400, IERC1400Capped {
      * @return Requested document + document hash.
      */
     function getDocument(bytes32 documentName) external override view returns (string memory, bytes32) {
-        require(bytes(_documents[documentName].docURI).length != 0, "Empty document");
-        // Action Blocked - Empty document
         return (
         _documents[documentName].docURI,
         _documents[documentName].docHash
