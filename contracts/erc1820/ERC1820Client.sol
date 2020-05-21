f7abb60e8a5aca521e061f85925f2edf34236888
Daniel Gretzke <daniel@gretzke.de>
2020-05-21 03:36:14 +0200

    commented unused functions

--- contracts/erc1820/ERC1820Client.sol
+++ contracts/erc1820/ERC1820Client.sol
@@ -25,12 +25,12 @@ contract ERC1820Client {
 		ERC1820REGISTRY.setInterfaceImplementer(address(this), interfaceHash, _implementation);
 	}
 
-	function interfaceAddr(address addr, string memory _interfaceLabel) internal view returns (address) {
-		bytes32 interfaceHash = keccak256(abi.encodePacked(_interfaceLabel));
-		return ERC1820REGISTRY.getInterfaceImplementer(addr, interfaceHash);
-	}
+	// function interfaceAddr(address addr, string memory _interfaceLabel) internal view returns (address) {
+	// 	bytes32 interfaceHash = keccak256(abi.encodePacked(_interfaceLabel));
+	// 	return ERC1820REGISTRY.getInterfaceImplementer(addr, interfaceHash);
+	// }
 
-	function delegateManagement(address _newManager) internal {
-		ERC1820REGISTRY.setManager(address(this), _newManager);
-	}
+	// function delegateManagement(address _newManager) internal {
+	// 	ERC1820REGISTRY.setManager(address(this), _newManager);
+	// }
 }
